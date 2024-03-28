be528ea84ffe09e6cbb913be4f2e5b849574286f
Jeongyong Lee <hua9378@gmail.com>
2024-03-28 18:53:54 +0900

    fix: 응답 코드 변경

--- src/main/java/live/ioteatime/apigateway/exception/TokenException.java
+++ src/main/java/live/ioteatime/apigateway/exception/TokenException.java
@@ -3,7 +3,7 @@ package live.ioteatime.apigateway.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class TokenException extends RuntimeException {
 
     public TokenException(String message) {
